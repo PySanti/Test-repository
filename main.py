9a3aea9b61a7ab28ad86422fec38e14639811d7d
santiago <santiagodeandrade4@gmail.com>
2025-03-15 19:44:54 -0400

    Prueba

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from numpy import diff
+from pandas.core.common import random_state
 from utils.basic_preprocess import basic_preprocess
 import pandas as pd
 import time
@@ -5,31 +7,36 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report
+from sklearn import datasets
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+
 
 
 
 t1 = time.time()
 [df_train, df_test, df_val] = basic_preprocess(pd.read_csv("./data/data.csv"),"HeartDisease")
-print(f"Tiempo de preprocesamiento {time.time() - t1}")
+print(f"Tiempo de preprocesamiento : {time.time()-t1}")
 
 
-
-svc = SVC()
-
-# Definición de los hiperparámetros a buscar
-param_grid = {
-    'C': [0.1, 1, 10, 100],
-    'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-    'gamma': ['scale', 'auto']
+opciones = {
+    94: {"max_depth":None, "min_samples_leaf":1,"min_samples_split":2, "n_estimators":400},
+    46: {"max_depth":None, "min_samples_leaf":1,"min_samples_split":2, "n_estimators":200},
+    72: {"max_depth":None, "min_samples_leaf":1,"min_samples_split":2, "n_estimators":300},
 }
-grid_search = GridSearchCV(estimator=svc, param_grid=param_grid, cv=5, scoring='accuracy', verbose=10, n_jobs=3)
 
-t1 = time.time()
-grid_search.fit(df_train.drop("HeartDisease", axis=1), df_train["HeartDisease"])
-print(f"Tiempo de entrenamiento total {time.time() - t1}")
+xs = []
 
-# Resultados
-print("Mejores hiperparámetros encontrados:")
-print(grid_search.best_params_)
+for t,hp in opciones.items():
+    print(f"Hiperparametros a estudiar : {hp}")
+    print(f"Segundos en pc de santiago: {t}")
+    t1 = time.time()
+    RandomForestClassifier(random_state=42, **hp).fit(df_train.drop("HeartDisease", axis=1), df_train["HeartDisease"])
+    t2 = time.time()
+    print(f"Segundos en pc de samuel {t2-t1}")
+    xs.append(t/(t2-t1))
 
+print(f"La pc de samuel es aproximadamente {sum(xs)/3} veces mas rapida que la de santiago")
 
